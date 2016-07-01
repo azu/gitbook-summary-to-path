bfab157fd2e7fb96965e42898ac6f7c5735e5408
azu <azuciao@gmail.com>
2016-07-02 00:04:27 +0900

    feat(lib): support nesting summary

--- test/fixtures/SUMMARY.md
+++ test/fixtures/SUMMARY.md
@@ -2,4 +2,6 @@
 
 * [Introduction](README.md)
 * [jQuery](jQuery.md)
+    * [nest1](nest1.md)
+        * [nest2](nest2.md)
 
--- test/gitbook-summary-to-path-test.js
+++ test/gitbook-summary-to-path-test.js
@@ -9,8 +9,12 @@ describe("#getFilePathListAsync", function () {
         return getFilePathListAsync(fixtureDir + "/SUMMARY.md").then(function (pathList) {
             var README = pathList[0];
             var jQuery = pathList[1];
+            var nest1 = pathList[2];
+            var nest2 = pathList[3];
             assert.equal(README, fixtureDir + "/README.md");
             assert.equal(jQuery, fixtureDir + "/jQuery.md");
+            assert.equal(nest1, fixtureDir + "/nest1.md");
+            assert.equal(nest2, fixtureDir + "/nest2.md");
         });
     });
 });
