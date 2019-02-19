5d2a15f053f8dd50f49e91de6d9beabad4070838
azu <azuciao@gmail.com>
2019-02-19 19:04:13 +0900

    feat(src): add getChapterListAsync method

--- test/gitbook-summary-to-path-test.js
+++ test/gitbook-summary-to-path-test.js
@@ -1,20 +1,87 @@
 // LICENSE : MIT
 "use strict";
 
-import {getFilePathListAsync} from '../';
+import {getFilePathListAsync, getChapterListAsync} from '../';
+
 var fixtureDir = __dirname + "/fixtures";
 var assert = require("power-assert");
-describe("#getFilePathListAsync", function () {
-    it("should return array of file path", function () {
-        return getFilePathListAsync(fixtureDir + "/SUMMARY.md").then(function (pathList) {
-            var README = pathList[0];
-            var jQuery = pathList[1];
-            var nest1 = pathList[2];
-            var nest2 = pathList[3];
-            assert.equal(README, fixtureDir + "/README.md");
-            assert.equal(jQuery, fixtureDir + "/jQuery.md");
-            assert.equal(nest1, fixtureDir + "/nest1.md");
-            assert.equal(nest2, fixtureDir + "/nest2.md");
+describe("gitbook-summary-to-path", () => {
+    describe("#getFilePathListAsync", function () {
+        it("should return array of file path", function () {
+            return getFilePathListAsync(fixtureDir + "/SUMMARY.md").then(function (pathList) {
+                var README = pathList[0];
+                var jQuery = pathList[1];
+                var nest1 = pathList[2];
+                var nest2 = pathList[3];
+                assert.equal(README, fixtureDir + "/README.md");
+                assert.equal(jQuery, fixtureDir + "/jQuery.md");
+                assert.equal(nest1, fixtureDir + "/nest1.md");
+                assert.equal(nest2, fixtureDir + "/nest2.md");
+            });
         });
     });
+    describe("#getChapterListAsync", function () {
+        it("should return array of chaper", function () {
+            return getChapterListAsync(fixtureDir + "/SUMMARY.md").then(function (pathList) {
+                assert.deepStrictEqual(pathList, [{
+                    "path": "README.md",
+                    "title": "Introduction",
+                    "level": "0",
+                    "articles": [],
+                    "exists": true,
+                    "external": false,
+                    "introduction": true
+                }, {
+                    "path": "jQuery.md",
+                    "title": "jQuery",
+                    "level": "1",
+                    "articles": [{
+                        "path": "nest1.md",
+                        "title": "nest1",
+                        "level": "1.1",
+                        "articles": [{
+                            "path": "nest2.md",
+                            "title": "nest2",
+                            "level": "1.1.1",
+                            "articles": [],
+                            "exists": true,
+                            "external": false,
+                            "introduction": false
+                        }],
+                        "exists": true,
+                        "external": false,
+                        "introduction": false
+                    }],
+                    "exists": true,
+                    "external": false,
+                    "introduction": false
+                }, {
+                    "path": "nest1.md",
+                    "title": "nest1",
+                    "level": "1.1",
+                    "articles": [{
+                        "path": "nest2.md",
+                        "title": "nest2",
+                        "level": "1.1.1",
+                        "articles": [],
+                        "exists": true,
+                        "external": false,
+                        "introduction": false
+                    }],
+                    "exists": true,
+                    "external": false,
+                    "introduction": false
+                }, {
+                    "path": "nest2.md",
+                    "title": "nest2",
+                    "level": "1.1.1",
+                    "articles": [],
+                    "exists": true,
+                    "external": false,
+                    "introduction": false
+                }])
+            });
+        });
+    })
+
 });
